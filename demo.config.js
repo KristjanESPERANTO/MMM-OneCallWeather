22f3fdb3e085078bb6a94dc74374335549b1876c
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-01-30 19:24:20 +0100

    chore: lint demo config

--- demo.config.js
+++ demo.config.js
@@ -1,120 +1,120 @@
 let config = {
-  address: "0.0.0.0",
+  address: '0.0.0.0',
   ipWhitelist: [],
-  logLevel: ["INFO", "LOG", "WARN", "ERROR", "DEBUG"],
+  logLevel: ['INFO', 'LOG', 'WARN', 'ERROR', 'DEBUG'],
   modules: [
     {
-      module: "clock",
-      position: "middle_center"
+      module: 'clock',
+      position: 'middle_center',
     },
 
     {
       disabled: true,
-      module: "MMM-OneCallWeather",
-      position: "top_right",
-      header: "TOTO",
+      module: 'MMM-OneCallWeather',
+      position: 'top_right',
+      header: 'TOTO',
       config:
                     {
                       latitude: 45.466667,
                       longitude: -73.75,
-                      apikey: "YOUR_API_KEY",
-                      iconset: "4a",
-                      iconsetFormat: "png",
-                      layout: "vertical"
-                    }
+                      apikey: 'YOUR_API_KEY',
+                      iconset: '4a',
+                      iconsetFormat: 'png',
+                      layout: 'vertical',
+                    },
     },
 
     {
       disabled: true,
-      module: "MMM-OneCallWeather",
-      header: "Weather in London",
-      position: "top_right",
+      module: 'MMM-OneCallWeather',
+      header: 'Weather in London',
+      position: 'top_right',
       config: {
         colored: false,
-        iconset: "9a",
-        iconsetFormat: "svg",
+        iconset: '9a',
+        iconsetFormat: 'svg',
 
-        latitude: "51.500149",
-        longitude: "-0.126240",
-        apikey: "YOUR_API_KEY"
-      }
+        latitude: '51.500149',
+        longitude: '-0.126240',
+        apikey: 'YOUR_API_KEY',
+      },
     },
     {
       disabled: true,
-      module: "MMM-OneCallWeather",
-      position: "top_left",
-      header: "TOTO",
+      module: 'MMM-OneCallWeather',
+      position: 'top_left',
+      header: 'TOTO',
       config: {
-        latitude: "45.466667",
-        longitude: "-73.75",
-        apikey: "YOUR_API_KEY",
-        iconset: "4a",
-        iconsetFormat: "png"
-      }
+        latitude: '45.466667',
+        longitude: '-73.75',
+        apikey: 'YOUR_API_KEY',
+        iconset: '4a',
+        iconsetFormat: 'png',
+      },
     },
     {
       disabled: true,
-      module: "MMM-OneCallWeather",
-      position: "top_left",
-      header: "TOTO",
+      module: 'MMM-OneCallWeather',
+      position: 'top_left',
+      header: 'TOTO',
       config: {
-        layout: "vertical",
-        latitude: "45.466667",
-        longitude: "-73.75",
-        apikey: "YOUR_API_KEY",
-        iconset: "4a",
-        iconsetFormat: "png"
-      }
+        layout: 'vertical',
+        latitude: '45.466667',
+        longitude: '-73.75',
+        apikey: 'YOUR_API_KEY',
+        iconset: '4a',
+        iconsetFormat: 'png',
+      },
     },
 
     {
       disabled: true,
-      module: "MMM-OneCallWeather",
-      position: "top_left",
-      header: "Weather in London",
+      module: 'MMM-OneCallWeather',
+      position: 'top_left',
+      header: 'Weather in London',
       config: {
-        latitude: "51.500149",
-        longitude: "-0.126240",
-        apikey: "YOUR_API_KEY",
-        displayMode: "both-vertical"
-      }
+        latitude: '51.500149',
+        longitude: '-0.126240',
+        apikey: 'YOUR_API_KEY',
+        displayMode: 'both-vertical',
+      },
     },
     {
       disabled: false,
-      module: "MMM-OneCallWeather",
-      position: "top_left",
-      header: "Weather in London",
+      module: 'MMM-OneCallWeather',
+      position: 'top_left',
+      header: 'Weather in London',
       config: {
-        layout: "vertical",
-        latitude: "51.500149",
-        longitude: "-0.126240",
-        apikey: "YOUR_API_KEY",
+        layout: 'vertical',
+        latitude: '51.500149',
+        longitude: '-0.126240',
+        apikey: 'YOUR_API_KEY',
         showCurrent: true,
         showForecast: true,
         showAlerts: true,
-        forecastLayout: "columns",
-        arrangement: "horizontal"
-      }
+        forecastLayout: 'columns',
+        arrangement: 'horizontal',
+      },
     },
     {
       disabled: false,
-      module: "MMM-OneCallWeather",
-      position: "top_right",
-      header: "Weather Alerts Test (Oklahoma)",
+      module: 'MMM-OneCallWeather',
+      position: 'top_right',
+      header: 'Weather Alerts Test (Oklahoma)',
       config: {
-        latitude: "35.4676",    // Oklahoma City (Tornado Alley)
-        longitude: "-97.5164",
-        apikey: "YOUR_API_KEY",
+        latitude: '35.4676',    // Oklahoma City (Tornado Alley)
+        longitude: '-97.5164',
+        apikey: 'YOUR_API_KEY',
         showCurrent: true,
         showForecast: false,
         showAlerts: true,       // Test alerts feature
-        colored: true
-      }
-    }
-  ]
-};
+        colored: true,
+      },
+    },
+  ],
+}
 
 /** ************* DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {
-  module.exports = config;
+if (typeof module !== 'undefined') {
+  module.exports = config
 }
